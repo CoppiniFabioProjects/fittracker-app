33a681251cb61a67b48984e35faef21a1baf79dd
Fabio Coppini <coppinifabio99@gmail.com>
2026-01-27 15:51:18 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Space Grotesk', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        dark: '#020617',
+      }
+    },
+  },
+  plugins: [],
+}
