285ea98d0d622051705d353b202bac2d6848c518
Fabio Coppini <coppinifabio99@gmail.com>
2026-01-27 14:44:01 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6LqmDyp30DoHbiFCpSxM2LNps_Md0WWQ",
+  authDomain: "fittracker-cyber.firebaseapp.com",
+  projectId: "fittracker-cyber",
+  storageBucket: "fittracker-cyber.firebasestorage.app",
+  messagingSenderId: "757340394897",
+  appId: "1:757340394897:web:585e102a70b7d307b4f630",
+  measurementId: "G-G6QZLNXCN3"
+};
